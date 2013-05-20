3c61f9b36307ba5f88006123e88514bdc34426b8
Adam Rensel <adamrensel@gmail.com>
2013-05-19 21:26:30 -0400

    Add first failing test

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,27 @@
-test('first test', function(){
-  equal(2+1, 4, '2+2 should equal 4')
+createConsole = function(options){
+  // var el = document.createElement('div')
+  var el = document.getElementById('console');
+  el.className = 'console'
+
+  return new CSConsole(el,{
+    prompt: options.prompt,
+    historyLabel: options.historyLabel,
+    syntax: options.syntax,
+    welcomeMessage: options.welcomeMessage,
+    autoFocus: options.autoFocus,
+    commandValidate: options.commandValidate,
+    commandHandle: options.commandHandle,
+    commandCancel: options.commandCancel
+  });
+}
+
+module('constructor options');
+test('setting prompt without welcome message should display prompt', function(){
+  var prompt = "> ";
+  var console = createConsole({
+    prompt: prompt
+  })
+  var cm = console.innerConsole();
+
+  ok( cm.getLine(cm.lineCount() - 1).match(new RegExp(prompt)) );
 });
